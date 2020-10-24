51709b6040fbe885d7450427e830c4827e812a6f
Marsallis <MarsallisF@gmail.com>
2020-10-24 17:33:01 -0400

    after class

---
+++ unit_1/w02d03/student_labs/morning_lab.js
@@ -0,0 +1,70 @@
+// const calculateCube = (num) => {
+//     console.log(num * num * num);
+// }
+
+// calculateCube(5);
+
+// const vowels = ["a", "e", "i", "o", "u"];
+
+// const isAVowel = (letter) => {
+//     letter = letter.toLowerCase();
+//     if (vowels.indexOf(letter) !== -1) {
+//         return true;     
+//     }
+//     return false;  
+    
+// }  
+
+// console.log(isAVowel("a"));
+
+// const returnName = () => {
+//     return 'Matt';
+// }
+
+// const returnGreeting = () =>{
+//     return `Hello ${returnName()}`;
+// }
+
+// console.log(returnGreeting());
+
+// const firstName = () => {
+//     return 'Marsallis';
+// }
+
+// const fullName = (lastName) => {
+//     return `${firstName()} ${lastName}`;
+// }
+
+// console.log(fullName(`Faircloth`));
+
+// for (var i=0; i < 100; i++) {
+//     console.log('Inside the block; ', i);
+// }
+
+// console.log('Outside the block: ', i);
+
+// const countdown = (num) => {
+//     if(num != 0){
+//         console.log(num);
+//         countdown(num - 2 );
+//     } else {
+//         return
+//     }
+// }
+
+// countdown(6);
+
+const getMultipleLengths = (arrayOfStrings) => {
+
+    let arrayStringLength = [];
+
+    for (let i = 0; i < arrayOfStrings.length; i++ ) {
+
+    arrayStringLength.push(arrayOfStrings[i].length);
+
+    }
+    return arrayStringLength;
+
+}
+
+console.log(getMultipleLengths(["hello", "what", "is", "up", "dude"]));
