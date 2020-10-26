a6047fa05030be993424be5cc16643f783f9c19b
Marsallis <MarsallisF@gmail.com>
2020-10-26 16:09:51 -0400

    arrays lab

---
+++ unit_1/w02d02/student_labs/arrays_conditionals_lab_solutions.js
@@ -0,0 +1,14 @@
+// Create an array that contains three quotes and store it in a variable called quotes.
+// var quotes = [ 
+//     "quote1",
+//     "quote2",
+//     "quote3"
+// ];
+
+// Given the following array const randomThings = [1, 10, "Hello", true]
+// how do you access the 1st element in the array?
+// console.log(randomThings[0]);
+
+//Change the value of "Hello" to "World".
+//    randomThings[2] = "World"
+
