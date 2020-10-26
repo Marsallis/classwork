cfb5a77c1af10a303fc1040a427a56c851b96e14
Marsallis <MarsallisF@gmail.com>
2020-10-26 18:13:01 -0400

    Prime Numbers

--- unit_1/w02d03/homework/solutions.js
+++ unit_1/w02d03/homework/solutions.js
@@ -38,5 +38,54 @@
 //Prime Numbers
 //////////////////////////////////////////////
 
+//prime number = can only be divided by itself or 1.
+
+//////////////
+//Step One
+//////////////
+
+// function isPrime(num){
+//     if(num <= 2){
+//         return false
+//     }
+
+//     for(let i = 2; i<num;i++){
+//         if(num % i === 0){
+//             return false;
+//         }
+//     }
+
+//     return true
+// }
+
+// console.log(isPrime(10));
+
+/////////////
+//Step Two
+/////////////
+
+// function printPrime(num){
+//     var temp = 0;
+//     for(var i=1; i <=num; i++){
+//         for(var j=2;j<=i-1;j++){
+//             if(i%j == 0){
+//                 temp += 1;
+//             }
+//         }
+//         if(temp == 0){
+//         }else{
+//             temp = 0;
+//         }
+//     }
+// }
+// console.log(printPrime(97))
+
+
+
+
+//////////////////////////////////////////////
+// Rock Paper Scissors
+//////////////////////////////////////////////
+
 
 
