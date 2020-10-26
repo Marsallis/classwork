445e4810ba4ff46e73a4ce63aba9fc9369c6fe18
Marsallis <MarsallisF@gmail.com>
2020-10-26 16:51:18 -0400

    palindrome completed

--- unit_1/w02d03/homework/solutions.js
+++ unit_1/w02d03/homework/solutions.js
@@ -1,17 +1,14 @@
 // Palindrome
 
 
-function checkPalindrome (str) {
-    return str == str.split('').reverse().join('');
+// const checkPalindrome = (string) => {
+//     string = string.toLowerCase();
+//     let reverseString = string.split("").reverse().join("");
+//     if ( string === reverseString ) {
+//         return true
+//      } else {
+//          return false
+//      }
+// }
+// console.log(checkPalindrome("radar"));
 
-    for( var i = palindrom.length; i > 0; i-- )
-    {
-        if( palindrom[i] = palindrom.charAt(palindrom.length)-1 )
-        {
-            document.write('the word is palindrome.');
-        }else{
-            document.write('the word is not palindrome!');
-        }
-    }
-}
-checkPalindrom('wordthatwillbechecked');
