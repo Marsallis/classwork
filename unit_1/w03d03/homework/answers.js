9b33cb140e83fdb56b03ddc12bb05ae2eb3cf219
Marsallis <MarsallisF@gmail.com>
2020-11-02 12:21:38 -0500

    finished homework

---
+++ unit_1/w03d03/homework/answers.js
@@ -0,0 +1,256 @@
+//ANSWER THE FOLLOWING
+
+
+
+//1.// datatype: boolean => ex: const lights = true
+
+
+
+
+//2.// datatype: string => ex: email = "email@gmail.com"
+
+
+
+
+//3.// datatype: object => ex: const spaceship = {hull, laser blasters,tractor beam, warp drive}
+
+
+
+
+//4.// datatype: array => ex: const students = [Sal, Arthur, Madeline, Bruno, Nando]
+
+
+
+
+//5.// datatype: object  => ex: const students = {name: Sal, location: NY}
+
+
+
+//6.// datatype: array inside of an object => ex: const students = {name: Sal, location: NY, favTVshows: ["Breaking Bad", "Power", "Game Of Thrones"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Take It Easy
+
+
+
+// const rainbow = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+// console.log(rainbow[4])
+
+
+
+
+// const me = {
+//     name: "Sal",
+//     favFood: "Mac n cheese",
+//     hobby: "Video Games",
+//     location: "Hyde Park",
+//     favDateType: "Objects"
+// }
+
+
+
+
+// console.log(me.hobby)
+
+
+
+
+
+
+
+
+
+
+//Crazy Object
+
+
+
+
+
+
+// const crazyObject = {
+//     taco: [{meat: 'steak',
+//            cheese: ['panela', 'queso', 'chihuahua']},
+//            {meat: 'chicken',
+//             salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//           ],
+//     larry: {
+//       nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//       quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//       characters: [{
+//         name: "Jeff",
+//         occupation: "manager"
+//       },
+//       {
+//         name: "funkhauser",
+//         occupation: "tv dude"
+//       },
+//       {
+//         name: "susie",
+//         occupation: "jeffs wife",
+//         favourtieHobby: "Swearing at Larry and Jeff"
+//       },
+//       ]
+//     }
+//   }
+
+
+// console.log(crazyObject.taco[1].salsa[5])
+
+
+// console.log(crazyObject.larry.quotes[0])
+
+
+// console.log(crazyObject.larry.characters[2].favourtieHobby)
+
+
+// console.log(crazyObject.larry.nicknames[1])
+
+
+// console.log(crazyObject.larry.characters[1])
+
+
+// crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk")
+
+
+// console.log(crazyObject.larry.quotes)
+
+
+
+
+
+
+
+
+
+
+//Object-ception
+
+
+
+
+// const inception = {
+//     reality: {
+//         dreamLayer1: {
+//             dreamLayer2: {
+//                 dreamLayer3: {
+//                     dreamLayer4: {
+//                         dreamLayer5: {
+//                             dreamLayer6: {
+//                                 limbo: "Joseph Gordon Levitt"
+//                            }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//  }
+
+
+
+
+//  inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6 = "null: 'Joseph Gordon Levitt'"
+
+
+
+// console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6)
+
+
+
+
+
+
+
+
+
+
+//Bond Films
+
+
+
+
+
+
+// const bondFilms = [
+//     { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
+//     { "title" : "Thunderball", "year" : 1965, "actor" : "Sean Connery", "gross" : "$1,014,941,117" },
+//     { "title" : "Goldfinger", "year" : 1964, "actor" : "Sean Connery", "gross" : "$912,257,512" },
+//     { "title" : "Live and Let Die", "year" : 1973, "actor" : "Roger Moore", "gross" : "$825,110,761" },
+//     { "title" : "You Only Live Twice", "year" : 1967, "actor" : "Sean Connery", "gross" : "$756,544,419" },
+//     { "title" : "The Spy Who Loved Me", "year" : 1977, "actor" : "Roger Moore", "gross" : "$692,713,752" },
+//     { "title" : "Casino Royale", "year" : 2006, "actor" : "Daniel Craig", "gross" : "$669,789,482" },
+//     { "title" : "Moonraker", "year" : 1979, "actor" : "Roger Moore", "gross" : "$655,872,400" },
+//     { "title" : "Diamonds Are Forever", "year" : 1971, "actor" : "Sean Connery", "gross" : "$648,514,469" },
+//     { "title" : "Quantum of Solace", "year" : 2008, "actor" : "Daniel Craig", "gross" : "$622,246,378" },
+//     { "title" : "From Russia with Love", "year" : 1963, "actor" : "Sean Connery", "gross" : "$576,277,964" },
+//     { "title" : "Die Another Day", "year" : 2002, "actor" : "Pierce Brosnan", "gross" : "$543,639,638" },
+//     { "title" : "Goldeneye", "year" : 1995, "actor" : "Pierce Brosnan", "gross" : "$529,548,711" },
+//     { "title" : "On Her Majesty's Secret Service", "year" : 1969, "actor" : "George Lazenby", "gross" : "$505,899,782" },
+//     { "title" : "The World is Not Enough", "year" : 1999, "actor" : "Pierce Brosnan", "gross" : "$491,617,153" },
+//     { "title" : "For Your Eyes Only", "year" : 1981, "actor" : "Roger Moore", "gross" : "$486,468,881" },
+//     { "title" : "Tomorrow Never Dies", "year" : 1997, "actor" : "Pierce Brosnan", "gross" : "$478,946,402" },
+//     { "title" : "The Man with the Golden Gun", "year" : 1974, "actor" : "Roger Moore", "gross" : "$448,249,281" },
+//     { "title" : "Dr. No", "year" : 1962, "actor" : "Sean Connery", "gross" : "$440,759,072" },
+//     { "title" : "Octopussy", "year" : 1983, "actor" : "Roger Moore", "gross" : "$426,244,352" },
+//     { "title" : "The Living Daylights", "year" : 1987, "actor" : "Timothy Dalton", "gross" : "$381,088,866" },
+//     { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
+//     { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
+// ];
+
+
+
+
+// const bondTitles = ["Skyfall", "Thunderball", "Goldfinger", "Live and let die", "You Only Live Twice", "The Spy Who Loved me", "Casino Royale", "Moonraker", "Diamonds Are Forever", "Quantum of Solace", "From Russia with Love", "Die Another Day", "Goldeneye", "On Her Majesty's Secret Service", "The World is Not Enough", "For Your Eyes Only", "Tomorrow Never Dies", "The Man with the Golden Gun", "Dr. No", "Octopussy", "The Living Daylights", "A View to a Kill", "License to Kill" ]
+
+
+
+
+
+// const oddBonds = ["Thunderball", "Live and Let Die", "You Only Live Twice", "The Spy Who Loved Me", "Moonraker", "Diamonds Are Forever", "From Russia With Love", "Goldeneye", "On her Majesty's Secret Service", "The World is Not Enough", "For Your Eyes Only", "Tomorrow Never Dies", "Octopussy", "The Living Daylights", "A View to a Kill", "License to Kill"]
+
+
+
+
+
+// bondFilms[0].gross = 1108561008
+// bondFilms[1].gross = 1014941117
+// bondFilms[2].gross = 912257512
+// bondFilms[3].gross = 825110761
+// bondFilms[4].gross = 756544419
+// bondFilms[5].gross = 692713752
+// bondFilms[6].gross = 669789482
+// bondFilms[7].gross = 655872400
+// bondFilms[8].gross = 648514469
+// bondFilms[9].gross = 622246378
+// bondFilms[10].gross = 576277964
+// bondFilms[11].gross = 543639638
+// bondFilms[12].gross = 529548711
+// bondFilms[13].gross = 505899782
+// bondFilms[14].gross = 491617153
+// bondFilms[15].gross = 486468881
+// bondFilms[16].gross = 478946402
+// bondFilms[17].gross = 448249281
+// bondFilms[18].gross = 440759072
+// bondFilms[19].gross = 426244352
+// bondFilms[20].gross = 381088866
+// bondFilms[21].gross = 321172633
+// bondFilms[22].gross = 285157191
+
+
+// gross = bondFilms[0].gross + bondFilms[1].gross + bondFilms[2].gross + bondFilms[3].gross + bondFilms[4].gross + bondFilms[5].gross + bondFilms[6].gross + bondFilms[7].gross + bondFilms[8].gross + bondFilms[9].gross + bondFilms[10].gross + bondFilms[11].gross + bondFilms[12].gross + bondFilms[13].gross + bondFilms[14].gross + bondFilms[15].gross + bondFilms[16].gross + bondFilms[17].gross + bondFilms[18].gross + bondFilms[19].gross + bondFilms[20].gross + bondFilms[21].gross + bondFilms[22].gross;
+// console.log(gross);
