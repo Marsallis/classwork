d715e50827cbc4bce3b88d823cc23491182b72c6
Marsallis <MarsallisF@gmail.com>
2020-11-04 18:35:12 -0500

    before class

--- unit_1/w03d03/student_examples/index.js
+++ unit_1/w03d03/student_examples/index.js
@@ -1,1 +1,60 @@
+// const adventurer = {
+//     name: "Arthur",
+//     hitpoints: 225,
+//     belongings: [
+//         {
+//             name: "excalibur",
+//             price: 1000000
+//         },
+//         {
+//             name: "the potion of healing",
+//             price: 250
+//         },
+//         {
+//             name: "tums",
+//             price: 20
+//         }
+//     ],
+//     getPrice(index){
+//         return this.belongings[index].price
 
+//     }
+// }
+
+// adventurer.belongings.push("sledgehammer");
+// // console.log(adventurer.belongings[0].price);
+
+// console.log(adventurer.getPrice(0));
+
+// for(let i = 0; i < adventurer.belongings.length; i++){
+//     console.log(adventurer.belongings[i].price)
+// }
+
+// console.log()
+
+
+
+
+const adventurer = {
+    name: "Kratos",
+    hitpoints: 500,
+    power: 60,
+    belongings: ['Levithian Axe', 'Blades of Chaos'],
+    companion: {
+        name: "Loki",
+        type: "Giant",
+        weapons: ['Talon Bow', 'Arrows', 'Fayes knife'],
+        companion: {
+            name: "Mimir",
+            type: "head",
+            powers: ['Magic Eyes', 'Superhuman Durability', 'wisdom']
+        }
+    }
+},
+attack(enemy) {
+    enemy.hitpoints = enemy.hitpoints - this.power
+    console.log(`I have attacked ${enemy.name} and they now have ${enemy.hitpoints}`)
+    if(enemy.hitpoints < 0) {
+        console.log(`${enemy.name} has been defeated`);
+    }
+},
