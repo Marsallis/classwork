9ab74228af1cf626acb0cc76d645976b5ca684b5
Marsallis <MarsallisF@gmail.com>
2020-11-14 10:02:24 -0500

    Before class

---
+++ unit_1/w05d01/homework/spaceships.js
@@ -0,0 +1,19 @@
+const USS = {
+    hull: 20,
+    firepower: 5,
+    accuracy: 0.7,
+}
+// Make class for alien ships with slightly different stats
+class alienShip {
+    constructor(hull, firepower, accuracy){
+    this.hull = Math.floor(Math.random() * 4) + 3;
+    this.firepower = Math.floor(Math.random() * 3) + 1;
+    this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
+}
+}
+console.log(new alienShip())
+Collapse
+
+
+
+
