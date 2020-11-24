2a5847d017ab52a0daca8e27e3d36fe6ff3153bc
Marsallis <MarsallisF@gmail.com>
2020-11-24 14:26:55 -0500

    chapter 10 completed

--- unit_1/w06d2/homework/LOTR/scripts/fellowship.js
+++ unit_1/w06d2/homework/LOTR/scripts/fellowship.js
@@ -238,10 +238,13 @@ const theBalrog = () => {
 const hornOfGondor = () => {
 
   // 1. create a pop-up alert that the horn of gondor has been blown
+  alert('the horn of gondor has been blown!');
 
   // 2. Boromir's been killed by the Uruk-hai! Put a linethrough on Boromir's name
+  $('.buddy').eq(4).css('text-decloration', 'line-through');
 
   // 3. Tricky: Remove the Uruk-Hai from the Baddies on the page
+  $('.baddy').eq(2).remove();
 
 };
 
