c12e7c6e5181697cba94571abb4017ad0f208c68
Marsallis <MarsallisF@gmail.com>
2021-01-13 18:43:09 -0500

    before class commit

---
+++ unit_2/w14d01/student_examples/fruits-mongoose-jsx/models/fruit.js
@@ -0,0 +1,10 @@
+const { SchemaType } = require("mongoose");
+
+const { model, Schema } = require('mongoose');
+const fruitSchema = new Schema({
+    name: { type: String, required: true },
+    color: String,
+    readyToEat: Boolean
+})
+const Fruit = model('Fruit', fruitSchema);
+module.exports = Fruit;
